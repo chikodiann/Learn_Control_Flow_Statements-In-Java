af1ff2cedae4a30b29afa135c5257411892eb59d
chikodiann <chikodiann@gmail.com>
2023-02-05 21:54:27 +0100

    Used primitive switch statement to determine quarters of the year

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //        } else {
 //            System.out.println("Was not 1 or 2");
 //        }
-         int switchValue = 7;
+         int switchValue = 2;
 
         switch (switchValue) {
             case 1 -> System.out.println("Value was 1");
@@ -21,5 +21,29 @@ public class Main {
             }
             default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
+
+        String month = "OCTOBER";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+    public static String getQuarter(String month) {
+         switch (month) {
+             case "JANUARY":
+             case "FEBRUARY":
+             case "MARCH":
+                 return "1st";
+             case "APRIL":
+             case "MAY":
+             case "JUNE":
+                 return "2nd";
+             case "JULY":
+             case "AUGUST":
+             case "SEPTEMBER":
+                 return "3rd";
+             case "OCTOBER":
+             case "NOVEMBER":
+             case "DECEMBER":
+                 return "4th";
+         }
+         return "bad";
     }
 }
