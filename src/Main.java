5b3618245b5da12dcd6eab0a2ac0c8fe44a5806f
chikodiann <chikodiann@gmail.com>
2023-02-05 21:34:23 +0100

    converted traditional switch statement to enhanced switch statement

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,14 @@ public class Main {
 //        }
          int switchValue = 7;
 
-         switch (switchValue) {
-             case 1:
-                 System.out.println("Value was 1");
-                 break;
-             case 2:
-                 System.out.println("Value was 2");
-                 break;
-             case 3: case 4: case 5:
-                 System.out.println("Was a 3, a 4,or a 5");
-                 System.out.println("Actually it was a " + switchValue);
-                 break;
-             default:
-                 System.out.println("Was not 1, 2, 3, 4, or 5");
-                 break;
-         }
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Was a 3, a 4,or a 5");
+                System.out.println("Actually it was a " + switchValue);
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
+        }
     }
 }
