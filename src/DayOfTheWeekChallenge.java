dcdd0198efe0214c9219c89ef52f72cdaec560e1
chikodiann <chikodiann@gmail.com>
2023-02-06 00:24:16 +0100

    wrote a code to print days of week using both enhanced switch method and if statements

---
+++ src/DayOfTheWeekChallenge.java
@@ -0,0 +1,61 @@
+public class DayOfTheWeekChallenge {
+    public static void main(String[] args) {
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+
+        System.out.println();
+        printDayOfWeek(9);
+
+        System.out.println();
+
+        printWeekDay(0);
+        printWeekDay(1);
+        printWeekDay(2);
+        printWeekDay(3);
+        printWeekDay(4);
+        printWeekDay(5);
+        printWeekDay(6);
+
+        System.out.println();
+        printWeekDay(7);
+    }
+    public static void printDayOfWeek(int day) {
+        String dayOfWeek = switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+    public static void printWeekDay(int day){
+        String dayOfWeek = String.valueOf(day);
+        if (day == 0) {
+            System.out.println("Sunday");
+        } else if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        }
+        else
+            System.out.println("Invalid Day");
+
+    }
+}
