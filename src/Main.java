75415f3e5cdd4901b35f20a7863162758ee000f1
chikodiann <chikodiann@gmail.com>
2023-02-05 21:55:53 +0100

    Converted primitive switch statement to determine quarters of the year to enhanced switch statement

--- src/Main.java
+++ src/Main.java
@@ -26,24 +26,12 @@ public class Main {
         System.out.println(month + " is in the " + getQuarter(month) + " quarter");
     }
     public static String getQuarter(String month) {
-         switch (month) {
-             case "JANUARY":
-             case "FEBRUARY":
-             case "MARCH":
-                 return "1st";
-             case "APRIL":
-             case "MAY":
-             case "JUNE":
-                 return "2nd";
-             case "JULY":
-             case "AUGUST":
-             case "SEPTEMBER":
-                 return "3rd";
-             case "OCTOBER":
-             case "NOVEMBER":
-             case "DECEMBER":
-                 return "4th";
-         }
-         return "bad";
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
     }
 }
