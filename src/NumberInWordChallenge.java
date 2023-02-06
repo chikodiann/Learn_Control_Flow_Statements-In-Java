c60753e3e54b24d9e33c6ee8da4a69df4bb5c7f2
chikodiann <chikodiann@gmail.com>
2023-02-06 09:37:19 +0100

    wrote a code to print days of week using both enhanced switch method and if statements

---
+++ src/NumberInWordChallenge.java
@@ -0,0 +1,29 @@
+public class NumberInWordChallenge {
+    public static void main(String[] args) {
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(2);
+        printNumberInWord(3);
+        printNumberInWord(-1);
+        printNumberInWord(10);
+    }
+    public static void printNumberInWord(int number) {
+        String printWord = switch (number){
+            case 0 -> "ZERO";
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> {
+                String badResponse = number + " OTHER";
+                yield badResponse;
+            }
+        };
+        System.out.println(number);
+    }
+}
